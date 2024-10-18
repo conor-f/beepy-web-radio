3a5de1033ed73ae018ad819ad6fa06afc1d79845
Conor Flynn <conor-f@users.noreply.github.com>
2024-10-18 15:48:36 +0200

    Adds a TUI (1.0.0) (#2)
    * Basic logging and splitting of the API.
    
    * Splits TUI to own file.
    
    * Functional TUI.
    
    * Adds j/k binding.
    
    * Bumps to 1.0.0

--- src/beepy_web_radio/main.py
+++ src/beepy_web_radio/main.py
@@ -1,21 +1,5 @@
-from textual.app import App, ComposeResult
-from textual.binding import Binding
-from textual.widgets import Footer, Label
-
 from .cli import main as cli_main
-
-
-class BeepyWebRadioApp(App):
-    BINDINGS = [
-        Binding("q", "quit", "Quit", show=True),
-    ]
-
-    def compose(self) -> ComposeResult:
-        yield Label("Beepy Web Radio")
-        yield Footer()
-
-    def on_mount(self) -> None:
-        pass
+from .tui import BeepyWebRadioApp
 
 
 def main() -> None:
